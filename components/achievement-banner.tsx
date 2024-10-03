60cf0d7a527873c61cb70c6ba61a64be75aa6aa4
tachyonok <tachyonparticle123@gmail.com>
2024-10-03 08:59:29 +0530

    margin-fix between title and subtitle

--- components/achievement-banner.tsx
+++ components/achievement-banner.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export function AchievementBanner() {
   return (
-    <div className="w-full bg-[#080808] h-full pt-20 pb-36">
+    <div className="w-full bg-[#080808] h-full pt-20 pb-40">
       <div className="max-w-full h-full px-4 md:px-16 py-12">
         <h1 className="text-4xl text-white md:text-5xl  lg:text-[96px] font-semibold mb-6 leading-tight">
           Diann achieves a score <br />
@@ -15,7 +15,7 @@ export function AchievementBanner() {
             standard.
           </span>
         </h1>
-        <p className="text-lg text-gray-300 lg:text-[40px] mt-12">
+        <p className="text-lg text-gray-300 lg:text-[40px] mt-24">
           Read our{" "}
           <Link href="#" className=" hover:underline">
             case study with OpenAI
